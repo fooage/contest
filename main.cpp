9cf77524718b8ec9cd8747cb966a35adda54f48b
fooage <nullptr.wang@gmail.com>
2020-11-10 23:17:41 +0800

    Made some changes

--- main.cpp
+++ main.cpp
@@ -3,15 +3,13 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 const int mod = 1e9 + 7;
-// ----Functions and global variables----
+const int inf = 0x7fffffff;
+// Never give up, keep going.
 
-// --------------------------------------
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
   cout.tie(0);
-  // ---------Input and output-----------
 
-  // ------------------------------------
   return 0;
 }
